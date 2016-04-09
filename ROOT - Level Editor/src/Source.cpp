5cb2fede16f0ffe6404d6671879c0bce7915ff26
boveloco <boveloco@gmail.com>
2016-04-09 18:29:12 -0300

    Changed to bytes changed the read/write to bytes

--- ROOT - Level Editor/src/Source.cpp
+++ ROOT - Level Editor/src/Source.cpp
@@ -4,8 +4,8 @@
 #include "Map.h"
 int main(int argc, char** argv) 
 {
-	//GamePlay game;
-	//game.Run();
+	/*GamePlay game;
+	game.Run();*/
 	
 	//testando spriteset
 	//SpriteSet* spriteS = new SpriteSet(10, 10, 1, 5);
@@ -13,11 +13,11 @@ int main(int argc, char** argv)
 	//std::cout << spriteS->getSprite(4)[0] << " && " << spriteS->getSprite(4)[1] << std::endl;
 	
 	//testando mapa
-	/*Map* m = new Map(10, 10);
+	/*Map* m = new Map(15, 200);
 	int * p = new int(2);
-	for (size_t i = 0; i < 10; i++)
+	for (size_t i = 0; i < 15; i++)
 	{
-		for (size_t j = 0; j < 10; j++)
+		for (size_t j = 0; j < 200; j++)
 		{
 			p[0] = i;
 			p[1] = j;
@@ -26,8 +26,8 @@ int main(int argc, char** argv)
 	}
 
 	m->print();
-	m->writeMap("map.dat");
-*/
+	m->writeMap("map.dat");*/
+
 	Map* n = new Map();
 	n = n->readMap("map.dat");
 	n->print();
