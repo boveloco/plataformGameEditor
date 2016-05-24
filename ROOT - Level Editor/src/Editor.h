477a9a97bf2ec6548159531f233fa98d0e4145f3
alissonads <alissond.ads@gmail.com>
2016-05-24 18:56:03 -0300

    ...

--- ROOT - Level Editor/src/Editor.h
+++ ROOT - Level Editor/src/Editor.h
@@ -1,1 +1,48 @@
 #pragma once
+#include"Scene.h"
+
+class SpriteSet;
+class Texture;
+class Map;
+class Camera;
+class Mouse;
+
+class Editor : public Scene
+{
+private:
+	bool m_quit;
+	char* mapa = "map1.dat";
+
+	SpriteSet* spriteSet;
+	Texture *m_texture;
+	Texture* hints;
+	Map* map;
+	Camera *m_camera;
+	Mouse* mouse;
+	SDL_Window* m_window;
+
+	//posição x e y da imagem
+	//namatriz de sprite
+	//para passar para o mause
+	int *m_index;
+
+	// id imagem que sera
+	//gravada na matriz
+	int img;;
+
+public:
+	Editor(SpriteSet*, Map*, SDL_Window*);
+	~Editor();
+
+	bool Quit();
+
+	virtual void SetEvent(SDL_Event &);
+
+	virtual void Initialize();
+	virtual void UpDate();
+	virtual void Draw();
+	//Desenha apenas o que a camera
+	//está vendo
+	void DrawOnCamera();
+	virtual void End();
+};
